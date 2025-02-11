692522ceb8030a49840a708afc5bcc5ed4792ed8
Eduardo Quadros <eduardosantanaquadros@gmail.com>
2025-02-10 21:42:16 -0300

    fix build

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { getChatById } from "@/domain/chat";
 import Chat, { ChatMessage } from "../components/chat";
-import { Message, Sender } from "@prisma/client";
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -10,22 +9,11 @@ export default async function ChatPage({ params }: Props) {
   const { id } = await params;
   const chat = await getChatById(id);
 
-  const chatMessages: ChatMessage[] =
-    (chat?.messages as Message[])?.map(
-      ({
-        content,
-        sender,
-        timestamp,
-      }: {
-        content: string;
-        sender: Sender;
-        timestamp?: Date;
-      }) => ({
-        content,
-        sender,
-        timestamp,
-      })
-    ) || [];
+  const chatMessages = chat?.messages.map(({ content, sender, timestamp }) => ({
+    content,
+    sender,
+    timestamp,
+  })) as ChatMessage[];
 
   return <Chat messages={chatMessages} chatId={id} />;
 }
